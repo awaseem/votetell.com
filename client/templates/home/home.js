67b11571f05c27bc47edc63a139c1e834765bb00
Ali Waseem <waseema393@gmail.com>
2015-07-01 22:32:37 -0600

    added dynamic form for choices

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -3,6 +3,14 @@
  */
 
 Template.home.events({
-    "click #test": function () {
+    "submit .new-question": function (event) {
+        var formData = $(".new-question").serializeArray();
+        console.log(formData);
+        $(".new-question").trigger("reset");
+        return false;
+    },
+    "focus .last-choice": function (event) {
+        $(".last-choice").removeClass("last-choice");
+        $(".submit").before("<input class='last-choice' type='text' name='choice' placeholder='Enter a choice'>");
     }
 });
